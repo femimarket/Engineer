4b087823593c4ae7f084ce25053637d4d39658ba
u <u@u.com>
2026-06-13 11:40:22 +0100

    Initial Commit

---
+++ ImageIterate2/ImageIterate2App.swift
@@ -0,0 +1,17 @@
+//
+//  ImageIterate2App.swift
+//  ImageIterate2
+//
+//  Created by u on 13/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ImageIterate2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
